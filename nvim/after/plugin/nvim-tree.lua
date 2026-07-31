48fbaff8ab26b3cd51f22fa5a152eb316723bfd1
Styxny <jacob.stickney@gmail.com>
2026-07-31 09:13:36 -0400

    Adjusting nvim-tree toggle

--- nvim/after/plugin/nvim-tree.lua
+++ nvim/after/plugin/nvim-tree.lua
@@ -1,4 +1,4 @@
 local api = require("nvim-tree.api")
 
-vim.keymap.set("n", "<leader>nt", api.tree.toggle, {desc = "NvimTree: Toggle"})
+vim.keymap.set("n", "<leader>t", api.tree.toggle, {desc = "NvimTree: Toggle"})
 
